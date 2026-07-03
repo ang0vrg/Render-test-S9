a496f1a5a68b90cbef162d96fa9316d8fcd447ba
Angel Vargas P. <angel.vargas.uni@gmail.com>
2026-07-03 12:33:02 -0500

    Para el main

--- src/test/java/com/worksync/worksync/WorksyncApplicationTests.java
+++ src/test/java/com/worksync/worksync/WorksyncApplicationTests.java
@@ -1,14 +1,21 @@
 package com.worksync.worksync;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.worksync.worksync.JWT.JwtUtil;
 
 @SpringBootTest
 class WorksyncApplicationTests {
 
+    @Autowired
+    private JwtUtil jwtUtil;
+
     @Test
     void contextLoads() {
+        String token = jwtUtil.generarToken("testuser@gmail.com", "COLABORADOR", 11L, "Test User");
+        System.out.println("TEST_JWT_TOKEN: " + token);
     }
-
 }
+
 
